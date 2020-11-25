3260993c5078dd4e09b852e109443f25d9924776
Jiaying <jgong1@babson.edu>
2020-11-25 10:02:56 +0800

    flask

--- project/app.py
+++ project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from forms import RegistrationFrom, LoginForm
 
 app = Flask(__name__)
@@ -6,16 +6,20 @@ app.config["SECERET_KEY"] = "1f249004b450cee67629fe240fa36f0a"
 
 
 @app.route("/")
-@app.route("/welcome/<name>")
-def welcome(name=None):
+@app.route("/home/<name>")
+def home(name=None):
     if name:
-        return render_template("welcome.html", name=name)
-    return "Welcome to your crypto tracking platform!"
+        return render_template("home.html", name=name)
+    else:
+        return "Welcome to your crypto tracking platform!"
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    form = RegistrationForm()
+    form = RegistrationFrom()
+    if form.validate_on_submit():
+        flash(f'Account Created for{form.username.data}!','success')
+        return redirect(url_for('home'))
     return render_template("register.html", title="Register", form=form)
 
 
@@ -23,3 +27,7 @@ def register():
 def register():
     form = LoginForm()
     return render_template("login.html", title="Login", form=form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
